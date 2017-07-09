a144244000a058cb6f7c2180151deebb07d27745
Julien Neuhart <j.neuhart@thecodingmachine.com>
2017-07-09 20:52:05 +0200

    wip: nginx + php-fpm

---
+++ .docker/php-fpm/.bin/usermod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+ROOT=${DIR}/../..;
+
+sedi()
+{
+    sed --version >/dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@";
+}
+
+# permissions issues workaround for Linux
+sedi "s/\${UID}/$(id -u)/g" ${ROOT}/.docker/docker-compose.yml;
+
+exit 0;
